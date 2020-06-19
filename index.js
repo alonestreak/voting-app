ab837762f396c58d992ba4e88a7a668b985b3714
Sushant Sanjay Sawant <sushantsawant865@gmail.com>
2020-06-19 18:24:46 +0530

    make it better

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ app.use((req, res, next) => {
 });
 app.use(handle.error);
 if (process.env.NODE_ENV==='production'){
-app.use(express.static('./client/build'))
+app.use(express.static('/client/build'))
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname, "client","build","index.html"))
+})
 }
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
